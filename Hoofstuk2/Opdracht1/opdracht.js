e6556b476400a31c1559ff5254fe1008ddddc66c
Timothy van Keulen <Maildx4@gmail.com>
2017-12-11 11:09:25 +0100

    Opdracht 2.1 & Opdracht 2.2

---
+++ Hoofstuk2/Opdracht1/opdracht.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "img/Kat1.png";
+document.getElementById("aap2").src = "img/Kat2.png";
---
+++ Hoofstuk2/Opdracht2/opdracht.js
@@ -0,0 +1,14 @@
+//Blokje 1
+bl1.style.backgroundColor = "cyan";
+bl1.style.textAlign = "center";
+//Blokje 2
+bl2.style.backgroundColor = "green";
+bl2.style.textAlign = "center";
+bl2.style.marginTop = "15px";
+//Blokje 3
+bl3.style.backgroundColor = "yellow";
+bl3.style.textAlign = "right";
+bl3.style.marginTop = "45px";
+//Blokje 4
+bl4.style.backgroundColor = "red";
+bl4.style.textAlign = "left";
